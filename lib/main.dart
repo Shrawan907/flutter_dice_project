0fa007d26f70ef273a112239a51c7e9b2adbadea
Shrawan907 <shrawan07090@gmail.com>
2019-12-16 21:55:59 +0530

    Normal increment

--- lib/main.dart
+++ lib/main.dart
@@ -16,41 +16,52 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int ldnumber = 1;
+  int rdnumber = 1;
   @override
   Widget build(BuildContext context) {
-    var ldnumber = 5;
-    var rdnumber = 4;
-    return Center(
 
+  return Center(
+    child: Row(
+    children: <Widget>[
+      Expanded(
+        child: FlatButton(
+        onPressed: (){
+        setState(() {
+          if(ldnumber == 6)
+            ldnumber = 1;
+          else
+            ldnumber += 1;
+        });
 
-      child: Row(
-        children: <Widget>[
-
-          Expanded(
-            child: FlatButton(
-              onPressed: (){
-
-                  ldnumber += 1;
-              },
-                child: Image.asset('images/dice$ldnumber.png'),
-            ),
-          ),
-
-          Expanded(
-            child: FlatButton(
-              onPressed: (){
-                if(rdnumber == 6)
-                  rdnumber = 1;
-                else
-                  rdnumber += 1;
-              },
-              child: Image.asset('images/dice$rdnumber.png'),
-            ),
-          ),
-
-        ],
+        },
+        child: Image.asset('images/dice$ldnumber.png'),
+        ),
       ),
-    );
+      Expanded(
+        child: FlatButton(
+        onPressed: (){
+          setState(() {
+            if(rdnumber == 6)
+              rdnumber = 1;
+            else
+              rdnumber += 1;
+          });
+
+        },
+        child: Image.asset('images/dice$rdnumber.png'),
+        ),
+      ),
+
+    ],
+    ),
+  );
   }
 }
+
