504af3bd0b808fd3a5cb2a3fbe02d2afc81fb56b
Shrawan907 <shrawan07090@gmail.com>
2019-12-16 21:26:27 +0530

    myfirst-commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
+          centerTitle: true,
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
@@ -18,6 +19,38 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    var ldnumber = 5;
+    var rdnumber = 4;
+    return Center(
+
+
+      child: Row(
+        children: <Widget>[
+
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+
+                  ldnumber += 1;
+              },
+                child: Image.asset('images/dice$ldnumber.png'),
+            ),
+          ),
+
+          Expanded(
+            child: FlatButton(
+              onPressed: (){
+                if(rdnumber == 6)
+                  rdnumber = 1;
+                else
+                  rdnumber += 1;
+              },
+              child: Image.asset('images/dice$rdnumber.png'),
+            ),
+          ),
+
+        ],
+      ),
+    );
   }
 }
